719df612ec69f8bf71e8e18dfd70e5ee679be41a
noname <peanutsch@duck.com>
2025-05-07 16:14:29 +0200

    favicon veranderd

--- game_result.php
+++ game_result.php
@@ -3,7 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="./images/favicon-32x32.png">
+
+    <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="./images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="./images/favicon/site.webmanifest">
+    
     <link rel="stylesheet" href="./style.css">
     <script src="./script.js" defer></script>
     
