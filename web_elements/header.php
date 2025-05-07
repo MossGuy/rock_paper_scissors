cf9f9e098a770113fba11a8b4552a4266c44e26c
noname <peanutsch@duck.com>
2025-05-07 09:34:30 +0200

    header en footer een apparte file gegeven. bestandsnamen veranderd voor taal consistentie. button styling compatible gemaakt met de .button class

---
+++ web_elements/header.php
@@ -0,0 +1,11 @@
+<header>
+        <section>
+            <h2>steen</h2>
+            <h2>papier</h2>
+            <h2>schaar</h2>
+        </section>
+        <section class="score_window">
+            <p>score</p>
+            <p id="score"></p>
+        </section>
+    </header>
